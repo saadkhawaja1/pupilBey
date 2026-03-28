31f1dd8b32cd2a042723fd075b9c0e2baf34becf
saadkhawaja1 <132588049+saadkhawaja1@users.noreply.github.com>
2026-03-28 20:02:33 +0500

    Initial PupilBey platform setup

---
+++ PupilBeyPlatform.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PupilBeyPlatform = () => {
+    return (
+        <div>
+            <h1>PupilBey Platform</h1>
+            <p>Welcome to the PupilBey platform!</p>
+        </div>
+    );
+};
+
+export default PupilBeyPlatform;
